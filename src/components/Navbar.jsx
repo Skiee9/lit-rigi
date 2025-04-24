f7ec10c68c1520cf20de252ff32da8a6b4689a73
skiee9 <chaudharyayushi785@gmail.com>
2025-04-24 13:11:07 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+         <nav style={{ padding: '1rem 2rem', background: '#000', color: '#fff' }}>
+      <h1 style={{ color: '#ff2b51' }}>🔥 Lit Vibes</h1>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar
